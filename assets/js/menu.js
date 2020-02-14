968ede09173be2c50a2dc3f422f0277fdad50607
KAMIL M <polska.17mil@o2.pl>
2020-02-14 14:12:30 +0100

    Refactoring code and add sticky nav

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,23 +1,40 @@
 var navHidden = false;
+var isSticky = false;
+var stickyNavTop;
 
-$(document).ready(function(){
-    
-    function  Hidding_nav(){
-        if($( window ).width()<=1435){
-            console.log("Hidenn");
-            $(".v-header-nav").addClass("is-hidden");
-            $("#hamburger").removeClass("is-hidden");
-            navHidden = true;
-        }else{
-            if(navHidden){
-                console.log("Visible!");
-                $("#hamburger").addClass("is-hidden");
-                $(".v-header-nav").removeClass("is-hidden");
-            }
+function  Hidding_nav(){
+    if($( window ).width()<=1435){
+        $(".v-header-nav").addClass("is-hidden");
+        $("#hamburger").removeClass("is-hidden");
+        navHidden = true;
+    }
+    else{
+        if(navHidden){
+            $("#hamburger").addClass("is-hidden");
+            $(".v-header-nav").removeClass("is-hidden");
+            navHidden = false;
         }
     }
+}
+
+function Sticky_nav(){
+    var scrollTop = $(window).scrollTop();      
+    if (scrollTop > stickyNavTop) { 
+        $('.v-header').addClass('is-sticky');
+        isSticky = true;
+        $(".v-header-nav").addClass("is-hidden");
+        navHidden = true;
+    } 
+    else {
+        $('.v-header').removeClass('is-sticky'); 
+        isSticky = false;
+    }
+};
+
+$(document).ready(function(){ 
+    stickyNavTop = $('.v-header').offset().top;
+    Hidding_nav();
     $("#hamburger").click(function(){
-        console.log("hamburger is clicked ! "+navHidden);
         if(navHidden){
             $(".v-header-nav").removeClass("is-hidden");
             navHidden = false;
@@ -29,6 +46,13 @@ $(document).ready(function(){
     });
     $( window ).resize(function() {
         Hidding_nav();
-      });
+        if(isSticky){
+            $('.v-header').removeClass('is-sticky'); 
+        }
+    }); 
+    $(window).scroll(function() {
+        if($( window ).width()<=1435){
+            Sticky_nav();
+        }
+    });    
 });
-
